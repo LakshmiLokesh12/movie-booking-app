889e0da922bf29e52a5d28507f52b24d78358e12
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-04-07 22:47:48 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+const serverConfig = require('./configs/server.config');
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
